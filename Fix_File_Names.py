68ef6f8a657caa332d879bd86c2f50e792e9a640
KGallik <kristin.gallik@gmail.com>
2025-11-25 19:37:06 -0500

    Created a new file name fix function and modified the rest of OIC-181_Mito_Analysis_300mM to work on the new dataset.

--- Fix_File_Names.py
+++ Fix_File_Names.py
@@ -33,3 +33,20 @@ def replace_spaces_with_underscores_recursive(directory):
                     dirs[i] = new_dirname
                 except OSError as e:
                     print(f"Error renaming directory {old_path}: {e}")
+
+
+def replace_extension(directory):
+    """
+    Replaces '.tiff' extension with '.tif' in the specified directory.
+    Does not search recursively.
+    """
+    for filename in os.listdir(directory):
+        if filename.endswith('.tiff'):
+            old_path = os.path.join(directory, filename)
+            new_filename = filename.replace('.tiff', '.tif')
+            new_path = os.path.join(directory, new_filename)
+            try:
+                os.rename(old_path, new_path)
+                print(f"Renamed file: {old_path} -> {new_path}")
+            except OSError as e:
+                print(f"Error renaming file {old_path}: {e}")
